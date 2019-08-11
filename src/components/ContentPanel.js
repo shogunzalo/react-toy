8cfc4c1255bca383951a632eee2471a1a7bd7f89
Gonzalo Rodriguez <g_rdo@live.cl>
2019-08-11 16:19:33 -0400

    Adding actions to buttons.

---
+++ src/components/ContentPanel.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+
+import Container from '@material-ui/core/Container';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const levelNames = ["Select your level", "Novice", "Advanced Beginner", "Practitioner", "Proficient", "Expert"];
+
+export default class ContentPanel extends Component {
+
+	constructor(props) {
+		super(props);
+		this.selectLevel = this.selectLevel.bind(this);
+		this.state = {
+			level: 0,
+			levelName: levelNames[0],
+      };
+    }
+
+    selectLevel(event, value) {
+      this.setState({
+        level: value ? value : 0,
+        levelName: value ? levelNames[value] : levelNames[0],
+      });
+    }
+
+    useStyles () {
+        return makeStyles(theme => ({
+		  button: {
+		    margin: theme.spacing(1),
+		  },
+		  input: {
+		    display: 'none',
+		  },
+		}));
+    }
+
+	render() {
+
+		const classes = this.useStyles();
+		const { expandedPanel, handleChange, identifier } = this.props;
+
+		return(
+			<ExpansionPanel 
+				key={ identifier } 
+				expanded={ expandedPanel === identifier } 
+				onChange={ handleChange.bind(this, identifier) }
+				onClick={this.handleClick}>
+				        <ExpansionPanelSummary
+				          expandIcon={<ExpandMoreIcon />}
+				          aria-controls="panel4bh-content"
+				          id="panel4bh-header"
+				        >
+			          	<Container>
+			          		<Typography>Panel Title</Typography>
+			          		<Container>
+			                  <br/>
+			                  <LinearProgress
+			                    variant="determinate"
+			                    color="secondary"
+			                    value={ this.state.level * 20 }
+			                  />
+			                  <Typography align="right">
+			                    { this.state.levelName }
+			                  </Typography>
+			                </Container>
+			          	</Container>
+			        </ExpansionPanelSummary>
+			        <ExpansionPanelDetails>
+			          <Container>
+			          	<Typography>
+							Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+							vitae egestas augue. Duis vel est augue.
+				        </Typography>
+				          <br/>
+				          <ToggleButtonGroup 
+							  exclusive
+							  value={this.state.level}
+					          size="small" 
+					          color="secondary" 
+					          variant="contained" 
+					          onChange={this.selectLevel}>
+						      <ToggleButton key={1} value="1">
+						        {levelNames[1]}
+						      </ToggleButton>
+						      <ToggleButton key={2} value="2">
+						        {levelNames[2]}
+						      </ToggleButton>
+						      <ToggleButton key={3} value="3">
+						        {levelNames[3]}
+						      </ToggleButton>
+						      <ToggleButton key={4} value="4">
+						        {levelNames[4]}
+						      </ToggleButton>
+						      <ToggleButton key={5} value="5">
+						        {levelNames[5]}
+						      </ToggleButton>
+					      </ToggleButtonGroup>
+						</Container>
+			        </ExpansionPanelDetails>
+			      </ExpansionPanel>
+		);
+	}
+}
--- src/components/SimplePanel.js
+++ src/components/SimplePanel.js
@@ -1,156 +1,35 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Container from '@material-ui/core/Container';
+
 import Grid from '@material-ui/core/Grid';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import { makeStyles } from '@material-ui/core/styles';
+import ContentPanel from './ContentPanel';
+import SidePanel from './SidePanel';
 
-import SidePanel from './SidePanel'
+const a = [1, 2, 3, 4, 5];
 
 export default class SimpleExpansionPanel extends Component {
 
 	constructor(props) {
-      super(props);
-      this.state = {
-        isLinearProgressVisible: false,
-        expandedPanel: ""
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {
+		expandedPanel: ""
       };
     }
 
-    useStyles () {
-        return makeStyles(theme => ({
-		  button: {
-		    margin: theme.spacing(1),
-		  },
-		  input: {
-		    display: 'none',
-		  },
-		}));
-    }
-
-    handleClick() {
+	handleChange (panel, event, expanded) {
       this.setState({
-        isLinearProgressVisible : !this.state.isLinearProgressVisible
-      });
-    }
-
-    handleChange = (panel, event, isExpanded) => {
-      this.setState({
-        expandedPanel: isExpanded ? panel : false,
+        expandedPanel: expanded ? panel : false,
       });
     }
 
 	render() {
-
-		const classes = this.useStyles();
-
 		return (
 			<Grid container spacing={3}>
 				<Grid item xs={7}>
-					<ExpansionPanel key="panelId1" expanded={this.state.expandedPanel === 'panel1'} onChange={this.handleChange.bind(this, "panel1")}>
-				        <ExpansionPanelSummary
-				          expandIcon={<ExpandMoreIcon />}
-				          aria-controls="panel4bh-content"
-				          id="panel4bh-header"
-				        >
-			          	<Container>
-			          		<Typography>Panel Title</Typography>
-				          	<Container>
-			                  <br/>
-			                  <LinearProgress
-			                    variant="determinate"
-			                    color="secondary"
-			                    value={10}
-			                  />
-			                  <Typography align="right">
-			                    label
-			                  </Typography>
-			                </Container>
-			          	</Container>
-			        </ExpansionPanelSummary>
-			        <ExpansionPanelDetails>
-			          <Container>
-			          	<Typography>
-							Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-							vitae egestas augue. Duis vel est augue.
-				        </Typography>
-				          <br/>
-				          <ButtonGroup size="small" color="secondary" variant="contained">
-						      <Button>
-						        Value 1
-						      </Button>
-						      <Button>
-						        Value 2
-						      </Button>
-						      <Button>
-						        Value 3
-						      </Button>
-						      <Button>
-						        Value 4
-						      </Button>
-						      <Button>
-						        Value 5
-						      </Button>
-					      </ButtonGroup>
-						</Container>
-			        </ExpansionPanelDetails>
-			      </ExpansionPanel>
-
-			      <ExpansionPanel expanded={this.state.expandedPanel === 'panel2'} onChange={this.handleChange.bind(this, "panel2")}>
-				        <ExpansionPanelSummary
-				          expandIcon={<ExpandMoreIcon />}
-				          aria-controls="panel2bh-content"
-				          id="panel2bh-header"
-				        >
-				        <Container>
-			          		<Typography>Panel Title</Typography>
-				          	<Container>
-			                  <br/>
-			                  <LinearProgress
-			                    variant="determinate"
-			                    color="secondary"
-			                    value={10}
-			                  />
-			                  <Typography align="right">
-			                    label
-			                  </Typography>
-			                </Container>
-			          	</Container>
-			        </ExpansionPanelSummary>
-			        <ExpansionPanelDetails>
-			          <Container>
-			          	<Typography>
-							Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-							vitae egestas augue. Duis vel est augue.
-				        </Typography>
-				          <br/>
-				          <ButtonGroup size="small" color="secondary" variant="contained">
-						      <Button>
-						        Value 1
-						      </Button>
-						      <Button>
-						        Value 2
-						      </Button>
-						      <Button>
-						        Value 3
-						      </Button>
-						      <Button>
-						        Value 4
-						      </Button>
-						      <Button>
-						        Value 5
-						      </Button>
-					      </ButtonGroup>
-						</Container>
-			        </ExpansionPanelDetails>
-			      </ExpansionPanel>
+					{a.map(i => {
+				        return <ContentPanel key={i} identifier={i} expandedPanel={this.state.expandedPanel} handleChange={this.handleChange}/>
+					})}
 				</Grid>
 				<Grid item xs={5}>
 					<SidePanel/>
